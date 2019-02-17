a41d9c30b02a3116adf18d1e1687f94fbb64c666
Irina Popa <irina.oana.popa@gmail.com>
2019-02-17 14:43:06 +0000

    k

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,7 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question.blank?
-      @answer = "I can't hear you!"
-    elsif @question =~ /i am going to work/i
+    if @question.match?(/i am going to work/i)
       @answer = "Great!"
     elsif @question.ends_with?("?")
       @answer = "Silly question, get dressed and go to work!"
@@ -12,5 +10,8 @@ class QuestionsController < ApplicationController
       @answer = "I don't care, get dressed and go to work!"
     end
   end
-
 end
+
+# action specifies the URL that will be used when submitting the form, so answer.html
+# and the action in the controller it will need
+# name enables you to name each parameter corresponding to each input of the form.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'questions#ask'
   get 'ask', to: 'questions#ask', as: :ask
   get 'answer', to: 'questions#answer', as: :answer
 end
